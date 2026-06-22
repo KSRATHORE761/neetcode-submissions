3ab3f936a4d6cc1e8937257ac65fc08bcd72ce9c
Kuldeep Rathore <rathore.kuldeep761@gmail.com>
2026-06-22 05:42:52 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        StringBuilder sb = new StringBuilder();
+        foreach(char ch in s){
+            if(char.IsLetterOrDigit(ch)){
+                sb.Append(ch);
+            }
+        }
+        string newWord= sb.ToString().ToLower();
+        int i=0;
+        int j= newWord.Length-1;
+        while(i<j){
+            if(newWord[i] != newWord[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
