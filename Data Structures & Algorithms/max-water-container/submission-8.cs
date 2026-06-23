edb88d04cff42e519aecd09d16b35d6c6f52fd8b
Kuldeep Rathore <rathore.kuldeep761@gmail.com>
2026-06-23 06:35:27 +0530

    Add: max-water-container - submission-8

---
+++ Data Structures & Algorithms/max-water-container/submission-8.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int maxWater = 0;
+        int l=0;
+        int r = heights.Length-1;
+        while(l<r){
+            maxWater = Math.Max(maxWater,((r-l) * Math.Min(heights[l],heights[r])));
+            if(heights[l]<heights[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return maxWater;
+    }
+}
